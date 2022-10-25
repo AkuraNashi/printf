b5df603ba7d7a35fbd09bf366bd0cf2605deb621
Leandro Camilo Da Costa <lcamilo-@c1r12s4.42lausanne.ch>
2022-10-25 22:55:33 +0200

    corrected error

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,37 @@
 /*   By: lcamilo- <lcamilo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 03:14:34 by lcamilo-          #+#    #+#             */
-/*   Updated: 2022/10/25 18:41:46 by lcamilo-         ###   ########.fr       */
+/*   Updated: 2022/10/25 22:54:50 by lcamilo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <limits.h>
+
+static int	ft_parsing(va_list args, const char c)
+{	
+	if (c == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	if (c == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	if (c == 'p')
+		return (ft_putstr("0x")
+			+ ft_putnbr_base(va_arg(args, unsigned long long),
+				"0123456789abcdef", 16));
+	if (c == 'd' || c == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	if (c == 'u')
+		return (ft_putunbr(va_arg(args, unsigned int)));
+	if (c == 'x')
+		return (ft_putnbr_base(va_arg(args, unsigned int),
+				"0123456789abcdef", 16));
+	if (c == 'X')
+		return (ft_putnbr_base(va_arg(args, unsigned int),
+				"0123456789ABCDEF", 16));
+	if (c == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
 
 int	ft_printf(const char *s, ...)
 {
@@ -25,35 +50,10 @@ int	ft_printf(const char *s, ...)
 	while (s[i])
 	{
 		if (s[i] == '%')
-		{
-			i++;
-			if (s[i] == 'c')
-				y += ft_putchar(va_arg(args, int));
-			else if (s[i] == 's')
-				y += ft_putstr(va_arg(args, char *));
-			else if (s[i] == 'p')
-			{
-				y += ft_putstr("0x");
-				y += ft_putnbr_base(va_arg(args, unsigned long long),
-						"0123456789abcdef", 16);
-			}
-			else if (s[i] == 'd' || s[i] == 'i')
-				y += ft_putnbr(va_arg(args, int));
-			else if (s[i] == 'u')
-				y += ft_putunbr(va_arg(args, unsigned int));
-			else if (s[i] == 'x')
-				y += ft_putnbr_base(va_arg(args, unsigned int),
-						"0123456789abcdef", 16);
-			else if (s[i] == 'X')
-				y += ft_putnbr_base(va_arg(args, unsigned int),
-						"0123456789ABCDEF", 16);
-			else if (s[i] == '%')
-				y += ft_putchar('%');
-			if (ft_strchr("cspdiuxX%", s[i]))
-				i++;
-		}
+			y += ft_parsing(args, s[i++ + 1]);
 		else
-			y += ft_putchar(s[i++]);
+			y += ft_putchar(s[i]);
+		i++;
 	}
 	va_end(args);
 	return (y);
